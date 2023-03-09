accc7fcd3b37624cfe8242414f434bf52a9d36e6
WebMelody <dev.mattew.chen@gmail.com>
2023-03-09 05:01:36 -0800

    changed project name

--- client/src/components/Nav/Nav-Brand/Navbrand.js
+++ client/src/components/Nav/Nav-Brand/Navbrand.js
@@ -5,7 +5,7 @@ const NavBrand = () => {
     return ( 
         <div href="#home" className='navbrand__container'>
            <h1 className='navbrand'>
-               <Link to="/">SHEMA</Link>
+               <Link to="/">MONTI</Link>
             </h1>
         </div>
      );
